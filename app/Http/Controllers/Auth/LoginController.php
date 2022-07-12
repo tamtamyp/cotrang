1a114a6f4918728754abb2cb896e537497792d3c
tamtamyp <thientamyp@gmail.com>
2022-07-12 20:27:30 +0700

    fix login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -31,6 +31,44 @@ class LoginController extends Controller
         session(['link' => url()->previous()]);
         return view('auth.login');
     }
+    public function login(Request $request)
+    {
+        $user = User::where('username', $request->username)->where('status',1)->first();
+        if($user){
+            $this->validateLogin($request);
+
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if (method_exists($this, 'hasTooManyLoginAttempts') &&
+            $this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        if ($this->attemptLogin($request)) {
+            if ($request->hasSession()) {
+                $request->session()->put('auth.password_confirmed_at', time());
+            }
+
+            return $this->sendLoginResponse($request);
+        }
+
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
+        }
+        else{
+            notify()->error("Tài khoản $request->username không tồn tại!", 'Error');
+            return redirect('/login');
+        }
+        
+    }
+
     protected function authenticated(Request $request)
     {
         $user = User::where('username',$request->username)->where('status','1')->first();
